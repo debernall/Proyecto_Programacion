ba341ed94c1998b18cb22faf23656d79ee33504a
jaabrilm <jaabrilm@unal.edu.co>
2021-05-26 17:52:00 -0500

    cambio a marco de referencia sobre la bola de cañon
    Se cambio el codigo para que ahora no se mueva la bola sino que se mueve el escenario llevando a la bola en su propio marco de refencia

--- proyecto.py
+++ proyecto.py
@@ -77,44 +77,45 @@ def intro_game(): #Pantalla de intro
 
 def rotate(surface, angle):
     rotated_surface=pygame.transform.rotozoom(surface,angle,1)
-    rotated_rect = rotated_surface.get_rect(center=(64,537))
+    rotated_rect = rotated_surface.get_rect(center=(400,350))
     return rotated_surface,rotated_rect
 
 def main():
     pygame.init()
-    screen= pygame.display.set_mode((948,720))
+    screen= pygame.display.set_mode((800,700))
     clock=pygame.time.Clock()
     pygame.display.set_caption('Titulo')
     
     #CARGA DE IMAGENES
     
-    plano = pygame.image.load("plano.jpg")          #Imagen de fondo
-    bola=pygame.image.load("bolacañon.png")       #Imagen de bala
-    cañon=pygame.image.load("cañonx.png")          #Imagen de cañon
-    recorte=pygame.image.load("planorecortado.jpg")    #Imagen recortada del fondo, que va entre el cañon y la bola para que no se vea la bola cuando no se ha disparado aun.
+    plano = pygame.image.load("enorme.jpg")          #Imagen de fondo
+    bola=pygame.image.load("bolacañonpequeña.png")       #Imagen de bala
+    cañon=pygame.image.load("cañon5.png")          #Imagen de cañon
+    cañonrecorte=cañon.get_rect(center=(400,350))
+    #recorte=pygame.image.load("planorecortado.jpg")    #Imagen recortada del fondo, que va entre el cañon y la bola para que no se vea la bola cuando no se ha disparado aun.
     explosion=pygame.image.load("explosión.png")    #imagen de la Explosón al disparar
     cuadro_angulo=pygame.Rect(0,0,250,50)
     letra_cuadro_angulo=pygame.font.SysFont('arial', 30) 
     
     #POSICION DE IMAGENES Y VARAIABLES A UTILIZAR
     
+    posplano=0,-1300
+    screen.blit(plano,posplano)
     
-    screen.blit(plano,(0,0))
-    
-    screen.blit(recorte,(0,473))                   #Carga la imagen de fondo
-   
-    screen.blit(cañon,(0,473))                     #Carga la imagen del cañon
+    #screen.blit(recorte,(0,473))                   #Carga la imagen de fondo
+    poscañon=336,286
+    #screen.blit(cañon,poscañon)                     #Carga la imagen del cañon
     pygame.display.flip()                           #Superpone las imagenes
     running=True                                    #Variable que mantiene activo el juego
-    pos= 0,473                                    #Declaración de posición inicial de la bala
-    pos1=-0,473                                   #Posición de la explosión antes de disparar
+    pos= -400,-350                                    #Declaración de posición inicial de la bala
+    pos1=-400,-350                                   #Posición de la explosión antes de disparar
     step= 0,0 
                                         #vector velocidad
-    angle=-22                                         #Declaración de variable ángulo del cañon
+    angle=0                                        #Declaración de variable ángulo del cañon
     speedangle=0                                    #Variable que almacena la rotación del cañon
                                                 
     n=0                                             #
-    v0=50                                       #Velocidad inicial
+    v0=150                                       #Velocidad inicial
     t=0                                             #Variable de tiempo
     while(running):
         ns=clock.tick(30)                           #Periodo de recarga de imagen
@@ -124,11 +125,13 @@ def main():
             
             elif event.type == pygame.KEYDOWN:      #Evento presionar tecla
                if event.key==pygame.K_SPACE:        #Tecla espacio 
-                   v_x0=v0*np.cos(np.radians(angle+22))                            #Velocidad inicial en x
-                   v_y0=-v0*np.sin(np.radians(angle+22))                           #Velocidad inicial en y(Es negativa porque el pixel (0,0) se encuentra en la esquina sup izq)           
+                   v_x0=v0*np.cos(np.radians(angle))                            #Velocidad inicial en x
+                   v_y0=-v0*np.sin(np.radians(angle))                           #Velocidad inicial en y(Es negativa porque el pixel (0,0) se encuentra en la esquina sup izq)           
                    step=v_x0,v_y0                   #Tras presionar la tecla espacio 
                    n=10/ns
-                   pos1=(150,400)                   #posición de la explosión al disparar
+                   pos= 400,350  
+                   
+                   pos1=(450,250)                   #posición de la explosión al disparar
                elif event.key==pygame.K_LEFT:       #Tecla izquierda rotación en sentido positivo
                    speedangle=1
                elif event.key==pygame.K_RIGHT:      #Tecla derecha rotación en sentido negativo
@@ -137,41 +140,45 @@ def main():
                    running = False
             
             elif event.type == pygame.KEYUP:        #Eventos dejar de presionar tecla
-               if event.key==pygame.K_SPACE:
-                   pos1=(0,473)
+               
+                    
                if event.key==pygame.K_LEFT:         #Dejar de presionar tecla izquierda detiene la rotación
                    speedangle=0
                elif event.key==pygame.K_RIGHT:      #Dejar de presionar tecla derecha detiene la rotación
                    speedangle=0
             
                 
-        screen.blit(plano,(0,0))                    #Carga la imagen de fondo tras cada paso del ciclo while
-        screen.blit(bola,pos)                     #Carga la imagen de la bala
-        pos=pos[0]+step[0],pos[1]+step[1]+((9.8/2)*(t**2))                            #(x,y)=(x0+v_x0,y0+v_y0)
+        screen.blit(plano,posplano)                    #Carga la imagen de fondo tras cada paso del ciclo while
+        #screen.blit(bola,pos)                     #Carga la imagen de la bala
+        posplano=posplano[0]-step[0],posplano[1]-step[1]-((9.8/2)*(t**2))                            #(x,y)=(x0+v_x0,y0+v_y0)
          
             
         
-        t=t+n
+        
         screen.blit(explosion,pos1)                 #carga imagen explosón disparar 
-        screen.blit(recorte,(0,473))                #carga imagen del recorte para que no se vea la bola antes de disprar 
+        pos1=pos1[0]-step[0],pos1[1]-step[1]-((9.8/2)*(t**2))
+        screen.blit(bola,pos)
+        #screen.blit(recorte,(0,473))                #carga imagen del recorte para que no se vea la bola antes de disprar 
         
         angle=angle+speedangle                      #Incrementa el ángulo del cañon de acuerdo a las teclas presionadas
       
         image2_rotated , image2_rotated_rect = rotate(cañon,angle)             #Rota el cañon
-        screen.blit(image2_rotated,image2_rotated_rect)                         #Carga la imagen del cañon rotado
+        poscañon=poscañon[0]-step[0],poscañon[1]-step[1]-((9.8/2)*(t**2))                            #(x,y)=(x0+v_x0,y0+v_y0)
+        screen.blit(image2_rotated,poscañon)
+                         
+        t=t+n
         
-       
         #REBOTES DE LA BOLA CUANDO IMPACTA CONTRA EL PISO O COSTADOS
         if pos[0]>948 or pos[0]<0 :
             step= -step[0],step[1]
          
-        if pos[1]>=540 :
+        if posplano[1]<=-1400 :
           #rebotar en la misma posición
           #step= 0,-step[1]-1
           #t=0
             
           #rebota y desliza horizontalmente  
-           pos=(pos[0],540)
+           posplano=(posplano[0],-1400)
           
            #no funciona igual que step=0,0
            #step[0]==0
@@ -185,11 +192,12 @@ def main():
            t=0
            n=0
             
-        crear_cuadro_de_texto(screen,cuadro_angulo,'Ángulo:'+str(angle),letra_cuadro_angulo,(0,0,0),(255,255,255))   #Agrega un cuadro de texto con el angulo.
+        crear_cuadro_de_texto(screen,cuadro_angulo,'Ángulo:'+str(angle+22),letra_cuadro_angulo,(0,0,0),(255,255,255))   #Agrega un cuadro de texto con el angulo.
        
             
                                     #El incremento 0.05 viene dado de la tasa tiempo_real/tiempo_maquina
-        print(angle+22)
+        
+        print(pos)
         pygame.display.flip()                                                   #Hace visibles las imagenes cargadas
 
         
