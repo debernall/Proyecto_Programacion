9a0d343d8abb84ed1c5335f0b9ade025653a3449
jaabrilm <79558952+jaabrilm@users.noreply.github.com>
2021-08-22 16:36:22 -0500

    elimine los mundos de más

--- proyecto.py
+++ proyecto.py
@@ -1065,7 +1065,7 @@ while jugar:
         puntos=0
         while jugar_outro:
             if nivel==0:
-                jugar_outro=mundo.main(space)
+                jugar_outro=mundo.main(marte2)
             elif nivel==1:
                 jugar_outro=mundo.main(luna)
             elif nivel==2:
