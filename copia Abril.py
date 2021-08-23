8c42b75ccb052675e2f6bcc465626fc5fc298159
jaabrilm <79558952+jaabrilm@users.noreply.github.com>
2021-08-23 08:30:35 -0500

    imagenes propuestas

--- copia Abril.py
+++ copia Abril.py
@@ -1053,7 +1053,7 @@ while jugar:
         puntos=0
         while jugar_outro:
             if nivel==0:
-                jugar_outro=mundo.main(luna)
+                jugar_outro=mundo.main(ganimedes)
             elif nivel==1:
                 jugar_outro=mundo.main(luna)
             elif nivel==2:
